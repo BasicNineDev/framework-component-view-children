1bacf074f2c8483b7e8f8f8932414d0556d2e358
hyoda <BasicNineDev>
2019-02-19 10:26:46 +0900

    @ViewChildren 에 대한 레퍼런스

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, QueryList, ViewChildren} from '@angular/core';
+import {ChildrenComponent} from './children/children.component';
+
+
+export interface Checkbox {
+  id: number;
+  label: string;
+  checked: boolean;
+}
+
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <h3>부모</h3>
+    <button type="button" (click)="toggle()">
+      자식 토글
+    </button>
+    <div *ngFor="let checkbox of checkboxes">
+      <children [checkbox]="checkbox"></children>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'framework-component-view-children';
+  checkboxes: Checkbox[] = [
+    {id: 1, label: 'HTML', checked: true },
+    {id: 2, label: 'CSS', checked: false },
+    {id: 3, label: 'Javascript', checked: false }
+  ];
+  active = false;
+
+  @ViewChildren(ChildrenComponent)
+  myChildren: QueryList<ChildrenComponent>;
+
+  toggle() {
+    this.active = !this.active;
+
+    /*
+     * 자식 컴포넌트들을 순회하며 자식 컴포넌트의 공개된 프로퍼티 checkbox를 변경한다.
+     * QueryList는 마치 자바스크립트 배열과 같이 사용할 수 있다.
+     */
+    this.myChildren.forEach(child => child.checkbox.checked = this.active);
+  }
 }
